b27138ddb646603e2ca0e7333b95b52a5081f8d4
Pechio <0ep40431326368y@gmail.com>
2023-04-03 20:56:18 +0900

    up

--- app.js
+++ app.js
@@ -1,59 +1,78 @@
+// ビディング履歴を格納する配列
+const biddingHistory = [];
+
+// 追加: BBOアラートテーブルを解析する関数
+function parseBBOalertTable(alertTableString) {
+  const table = {};
+
+  alertTableString.split(';').forEach((alertString) => {
+    const [bidString, alert] = alertString.split('=');
+    const bids = bidString.split('/');
+    bids.forEach((bid) => {
+      table[bid.trim()] = alert.trim();
+    });
+  });
+
+  return table;
+}
+
+// 追加: ビディングシステムの文字列を解析する関数
+function parseBiddingSystem(systemString) {
+  const system = {};
+  const lines = systemString.split('\n');
+
+  lines.forEach((line) => {
+    const [bid, response] = line.split(',');
+    system[bid.trim()] = response.trim();
+  });
+
+  return system;
+}
+
+// 追加: ビディング内容を表示する関数
+function showBidContent(bidContent) {
+  const bidContentElement = document.getElementById('bid-content');
+  bidContentElement.textContent = bidContent;
+}
+
+// ボタンがクリックされたときの処理
+function handleBidButtonClick(event) {
+  const bid = event.target.getAttribute('data-bid');
+  const alert = biddingTable[bid];
+
+  if (alert) {
+    addToBiddingHistory(2, bid, alert);
+  } else {
+    addToBiddingHistory(2, bid);
+  }
+
+  // 追加: ビディング内容を表示
+  const bidContent = biddingSystem[bid];
+  if (bidContent) {
+    showBidContent(bidContent);
+  } else {
+    showBidContent('');
+  }
+}
+
+// BBOアラートテーブルの文字列
+const bboalertString = '1C=12-14bal/12+!C/18+any; 1D=0-6(7); any/8-11 unbal no 4M';
+
+// BBOアラートテーブルを解析
+const biddingTable = parseBBOalertTable(bboalertString);
+
+// ビディングシステムの文字列
+const biddingSystemString = `1C, 12-14bal/12+!C/18+any
+1D, 0-6(7); any/8-11 unbal no 4M`;
+
+// ビディングシステムを解析
+const biddingSystem = parseBiddingSystem(biddingSystemString);
+
+// ボタンのイベントリスナーを設定
 document.addEventListener('DOMContentLoaded', () => {
-    // ここに対話機能のコードを書く
+  const bidButtons = document.querySelectorAll('.bid-button');
 
-    const biddingHistory = [];
-
-    const biddingForm = document.getElementById('bidding-form');
-    biddingForm.addEventListener('submit', handleBidding);
-
-    function handleBidding(event) {
-        event.preventDefault();
-        const bidInput = document.getElementById('bid');
-        const bid = bidInput.value;
-
-        // バリデーションとエラーチェックを実装する（オプション）
-
-        // ビディング履歴に追加
-        biddingHistory.push({ player: 'South', bid });
-
-        // ビディングテーブルを更新
-        updateBiddingTable();
-
-        // 入力をクリア
-        bidInput.value = '';
-    }
-
-    function updateBiddingTable() {
-        const biddingTableBody = document.querySelector('#bidding-table tbody');
-        biddingTableBody.innerHTML = '';
-
-        biddingHistory.forEach((bidData, index) => {
-            if (index % 4 === 0) {
-                const newRow = biddingTableBody.insertRow();
-                newRow.insertCell().textContent = bidData.bid;
-            } else {
-                const lastRow = biddingTableBody.rows[biddingTableBody.rows.length - 1];
-                lastRow.insertCell().textContent = bidData.bid;
-            }
-        });
-    }
-
-    function isValidBid(bid) {
-        const validBidPattern = /^([1-7][♣♦♥♠]|pass|double|redouble)$/i;
-        return validBidPattern.test(bid);
-      }
-      
-      function handleBidding(event) {
-        event.preventDefault();
-        const bidInput = document.getElementById('bid');
-        const bid = bidInput.value;
-      
-        if (!isValidBid(bid)) {
-          alert('無効なビディングです。');
-          return;
-        }
-      
-        // 以降の処理は同じ
-      }
-
+  bidButtons.forEach((button) => {
+    button.addEventListener('click', handleBidButtonClick);
+  });
 });
