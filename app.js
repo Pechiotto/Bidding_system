7aff6dd99a490b4dc9d3e15a0d82ec78782992a1
Pechio <0ep40431326368y@gmail.com>
2023-04-05 22:28:59 +0900

    up

--- app.js
+++ app.js
@@ -1,123 +1,79 @@
-// ビディング履歴を格納する配列
-const biddingHistory = [];
 
-// 追加: BBOアラートテーブルを解析する関数
-function parseBBOalertTable(alertTableString) {
-  const table = {};
-
-  alertTableString.split(';').forEach((alertString) => {
-    const [bidString, alert] = alertString.split('=');
-    const bids = bidString.split('/');
-    bids.forEach((bid) => {
-      if (bid && alert) {
-        table[bid.trim()] = alert.trim();
-      }
-    });
-  });
-
-
-  return table;
-}
-
-// 追加: ビディングシステムの文字列を解析する関数
-function parseBiddingSystem(systemString) {
-  const system = {};
-  const lines = systemString.split('\n');
-
-  lines.forEach((line) => {
-    const [bid, response] = line.split(',');
-    if (bid && response) {
-      system[bid.trim()] = response.trim();
-    }
-  });
-
-
-  return system;
-}
-
-// 追加: ビディング内容を表示する関数
-function showBidContent(bidContent) {
-  const bidContentElement = document.getElementById('bid-content');
-  bidContentElement.textContent = bidContent;
-}
-
-// ボタンがクリックされたときの処理
-function handleBidButtonClick(event) {
-  const bid = event.target.getAttribute('data-bid');
-  const alert = biddingSystemString[bid];
-
-  if (alert) {
-    addToBiddingHistory(2, bid, alert);
-  } else {
-    addToBiddingHistory(2, bid);
-  }
-
-  // 追加: ビディング内容を表示
-  const bidContent = biddingSystem[bid];
-  if (bidContent) {
-    showBidContent(bidContent);
-  } else {
-    showBidContent('');
-  }
-}
-
-function updateBiddingTable() {
-    const biddingTableBody = document.getElementById('bidding-table').querySelector('tbody');
-    biddingTableBody.innerHTML = '';
-  
-    biddingHistory.forEach((bidItem, index) => {
-      const row = document.createElement('tr');
-      const playerCell = document.createElement('td');
-      playerCell.textContent = bidItem.player;
-      row.appendChild(playerCell);
-  
-      const bidCell = document.createElement('td');
-      bidCell.textContent = bidItem.bid;
-      row.appendChild(bidCell);
-  
-      const alertCell = document.createElement('td');
-      if (bidItem.alert) {
-        alertCell.textContent = bidItem.alert;
-      } else {
-        alertCell.textContent = '-';
-      }
-      row.appendChild(alertCell);
-  
-      biddingTableBody.appendChild(row);
-  
-      if ((index + 1) % 4 === 0) {
-        const separatorRow = document.createElement('tr');
-        const separatorCell = document.createElement('td');
-        separatorCell.colSpan = 4;
-        separatorRow.appendChild(separatorCell);
-        biddingTableBody.appendChild(separatorRow);
-      }
-    });
-  }
-
-  function addToBiddingHistory(player, bid, alert) {
-    const bidItem = {
-      player: player,
-      bid: bid,
-      alert: alert
-    };
-    biddingHistory.push(bidItem);
-    updateBiddingTable(); // 追加
-  }
-  
-  
-
-// ビディングシステムの文字列
-const biddingSystemString = `1C, 12-14bal/12+!C/18+any; 1D, 0-6(7) any/8-11 unbal no 4M`;
-
-// ビディングシステムを解析
-const biddingSystem = parseBiddingSystem(biddingSystemString);
-
-// ボタンのイベントリスナーを設定
 document.addEventListener('DOMContentLoaded', () => {
-  const bidButtons = document.querySelectorAll('.bid-button');
-
+  const bidButtons = document.querySelectorAll('[data-bid]');
   bidButtons.forEach((button) => {
-    button.addEventListener('click', handleBidButtonClick);
+    button.addEventListener('click', () => {
+      showResponderTable(button.dataset.bid);
+    });
   });
 });
+
+const responseTables = {
+  '1C':
+  {
+    title: '1C Responder Bids',
+    bids: [
+      { bid: '1♦', description: '0-6(7); any/8-11 unbal no 4M' },
+      { bid: '1♥', description: '7+; 4+!H' },
+      { bid: '1♠', description: '7+; 4+!S' },
+      { bid: '1N', description: '8-10; bal,no 4M' }
+    ]
+  },
+  '1D': {
+    title: '1D Responder Bids',
+    bids: [
+      { bid: '1♥', description: '4+!H' },
+      { bid: '1♠', description: '4+!S' },
+      { bid: '1N', description: '7-10; possible 6!C' },
+      { bid: '2♣', description: '10+; 5+!C' }
+    ]
+  }
+};
+
+function showResponderTable(openerBid) {
+  const biddingTable = document.getElementById('biddingTable');
+  const responseTableData = responseTables[openerBid];
+  if (!responseTableData) return;
+
+  let tableHtml = `
+    <tr>
+      <th>bid</th>
+      <th>description</th>
+    </tr>
+  `;
+
+  for (const response of responseTableData.bids) {
+    tableHtml += `
+      <tr>
+        <td><button class="${suitClass(response.bid)}" onclick="showNextResponseTable('${response.bid}')">${response.bid}</button></td>
+        <td>${response.description}</td>
+      </tr>
+    `;
+  }
+
+  biddingTable.innerHTML = tableHtml;
+}
+
+
+
+function suitClass(bid) {
+  const suit = bid.slice(-1);
+  switch (suit) {
+    case '♣':
+      return 'club';
+    case '♦':
+      return 'diamond';
+    case '♥':
+      return 'heart';
+    case '♠':
+      return 'spade';
+    default:
+      return '';
+  }
+}
+
+function showNextResponseTable(responseBid) {
+  // 次の応答のテーブルを表示するためのロジックをここに追加してください。
+}
+
+
